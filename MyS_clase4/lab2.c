4b7e3807d90cd506b2990a4d2cb91f8604d5a46e
Carlos Maffrand <carlosmaffrand5@gmail.com>
2022-05-16 22:50:31 -0300

    Modifies lab2.c to handle outputs.

--- MyS_clase4/lab2.c
+++ MyS_clase4/lab2.c
@@ -1,32 +1,47 @@
 #include "xparameters.h"
+#include "xil_printf.h"
 #include "xgpio.h"
- 
+#include "xil_types.h"
+
+#define SW_CHANNEL 1
+#define BTN_CHANNEL 2
+#define LED_CHANNEL 1
 
 //====================================================
 
-int main (void) 
+int main (void)
 {
+	XGpio_Config *cfg_ptr;
+    XGpio dip, led;
+	int dip_check, psb_check;
+	u32 count;
 
-    XGpio dip, push;
-	int psb_check, dip_check;
-	
     xil_printf("-- Start of the Program --\r\n");
- 
-    XGpio_Initialize(&dip, XPAR_SWITCHES_DEVICE_ID);
-	XGpio_SetDataDirection(&dip, 1, 0xffffffff);
-	
-	XGpio_Initialize(&push, XPAR_BUTTONS_DEVICE_ID);
-	XGpio_SetDataDirection(&push, 1, 0xffffffff);
-	
+
+    XGpio_Initialize(&dip, XPAR_AXI_GPIO_0_BASEADDR);
+	XGpio_SetDataDirection(&dip, SW_CHANNEL, 0xffffffff);
+	XGpio_SetDataDirection(&dip, BTN_CHANNEL, 0xffffffff);
+
+	cfg_ptr = XGpio_LookupConfig(XPAR_AXI_GPIO_1_BASEADDR);
+	XGpio_CfgInitialize(&led, cfg_ptr, cfg_ptr->BaseAddress);
+	XGpio_SetDataDirection(&led, LED_CHANNEL, 0);
+
+	count = 32;
+
 	while (1)
 	{
-	  psb_check = XGpio_DiscreteRead(&push, 1);
-	  xil_printf("Push Buttons Status %x\r\n", psb_check);
-	  dip_check = XGpio_DiscreteRead(&dip, 1);
-	  xil_printf("DIP Switch Status %x\r\n", dip_check);
-	  
-	  sleep(1);
+
+	  dip_check = XGpio_DiscreteRead(&dip, SW_CHANNEL);
+	  xil_printf("DIP Switch Status %x\r", dip_check);
+
+	  psb_check = XGpio_DiscreteRead(&dip, BTN_CHANNEL);
+	  xil_printf("Buttons Status %x\r", psb_check);
+
+	  XGpio_DiscreteWrite(&led, LED_CHANNEL, count);
+	  xil_printf("Output to Leds %x\r", count);
+	  count = XGpio_DiscreteRead(&led, LED_CHANNEL) + 1;
+
+	  sleep(2);
 	}
- 
+
 }
- 
